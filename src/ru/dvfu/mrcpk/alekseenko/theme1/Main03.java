69d1fb01748e553193929d7c06e860198fbd3ce1
lazray <lazray@mail.com>
2018-11-02 20:25:01 +1000

    тест

--- src/ru/dvfu/mrcpk/alekseenko/theme1/Main03.java
+++ src/ru/dvfu/mrcpk/alekseenko/theme1/Main03.java
@@ -1,4 +1,20 @@
 package ru.dvfu.mrcpk.alekseenko.theme1;
+// задача нахождения следующего четного числа
+import java.util.Scanner;
 
 public class Main03 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите значение целое натуральное число ");
+        // Вводимое значение
+        int x = in.nextInt();
+
+        //Вычисление четного числа
+        int y = ((x / 2) * 2 + 2);
+
+        // Приглашение ввести данных
+        System.out.println("Четное число " + y);
+    }
 }
